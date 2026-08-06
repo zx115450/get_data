aafd6b4077b471a3ffb1c7e6560d9b28fc995598
zxhzzzsdsb <242917859@qq.com>
2026-08-06 17:33:07 +0800

    优化

--- problems/雷德王还有三个小时降临地球_世界各大媒体平台将这条消息传得沸沸扬扬_他的到来_是_20260802_161456/std.cpp
+++ problems/雷德王还有三个小时降临地球_世界各大媒体平台将这条消息传得沸沸扬扬_他的到来_是_20260802_161456/std.cpp
@@ -1,116 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
-#define lb(x) ((x) & (-x))
-#define fori(a, b) for (int i = a; i <= b; i++)
-#define forj(a, b) for (int j = a; j <= b; j++)
-#define for_(x, a, b) for (int x = a; x <= b; x++)
-#define refi(a, b) for (int i = b; i >= a; i--)
-#define all(a) a.begin(), a.end()
-#define all1(a) a.begin() + 1, a.end()
-#define getp(a, index) get<index>(a)
-#define endl '\n'
-#define grt greater<int>()
-using ll = long long;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
-typedef vector<vector<int>> vii;
-typedef tuple<int, int, int> piii;
-typedef tuple<int, int, int, int> piiii;
-typedef pair<double, double> pdd;
-typedef long long ll;
-typedef double db;
-typedef unsigned long long ull;
-typedef long double ldb;
-template <typename T, typename... Args>
-void print(T first, Args... args) {
-	std::cout << first << " ";
-	print(args...);
-}
-struct cmp {
-	bool operator()(pii as, pii bs) {
-		return as.second - as.first < bs.second - bs.first;
+
+using i64 = long long;
+const i64 INF = -1; // 表示不存在，输出用 -1
+
+// 返回 g(p,k)；不存在返回 -1
+i64 calc_g(i64 p, i64 k) {
+	// x0 = ceil(p/k)*k
+	i64 x0;
+	if (p == 0) {
+		x0 = 0;
+	} else {
+		// (p+k-1)/k * k，注意防溢出用 __int128
+		__int128 num = (__int128)p + k - 1;
+		x0 = (i64)((num / k) * k);
 	}
-};
-const int N = 200010;
-int mod = 998244353;
-int fan[4][2] = {1, 0, 0, 1, -1, 0, 0, -1};
-void man() {
-		
-	int n,m,k; cin>>n>>m>>k;
-	vector<vector<int>> g(n+1);
-	for (int i=1;i<=m;i++) {
-		int u,v; cin>>u>>v;
-		g[u].push_back(v);
-		g[v].push_back(u);
+
+	if (((x0 - p) & 1) == 0) {
+		return x0;
 	}
-	vector<vector<int>> p(n+1 , vector<int>(2 , 1e18));
-//	vector<int> sti(n+1);
-	queue<piii> dq;
-    p[1][0]=0;
-	dq.push({1 , 0 , -1});
-//	sti[1] = 1;
-	while(!dq.empty()){
-		auto [x , dist , pre] = dq.front();
-		dq.pop();
-		if (p[x][dist%2] < dist) {
-			continue;
-		}
-		
-		for (int y : g[x]) {
-			if (y == pre) {
-				continue;
-			}
-            int nd=dist+1;
-            if(p[y][nd%2]>nd){
-                p[y][nd%2]=nd;
-                dq.push({y , dist + 1 , x});
-            }
-		}
+	// x0-p 为奇数
+	if (k & 1) {
+		// 再加一个 k，可能溢出到 > 2^63-1？x0+k ≤ (p+k-1)+k < 3e18，仍在 i64 内
+		return x0 + k;
 	}
-	cout<<0<<' ';
-	for (int i=2;i<=n;i++) {
-		ll res=1e18;
-        if(p[i][0]!=1e18){
-            ll x=(p[i][0]+k-1)/k*k;
-            if((x-p[i][0])%2!=0){
-                if(k&1){
-                    x+=k;
-                }
-                else{
-                    x=1e18;
-                }
-            }
-            res=min(res,x);
-        }
-        if(p[i][1]!=1e18){
-            ll x=(p[i][1]+k-1)/k*k;
-            if((x-p[i][1])%2!=0){
-                if(k&1){
-                    x+=k;
-                }
-                else{
-                    x=1e18;
-                }
-            }
-            res=min(res,x);
-        }
-        if(res>=1e18) cout<<"-1 ";
-        else cout<<res<<' ';
-    }
-    cout<<endl;
+	return -1;
 }
 
-signed main() {
-	cout << fixed << setprecision(2);
+i64 solve_one(i64 p0, i64 p1, i64 k) {
+	i64 ans = -1;
+	auto upd = [&](i64 p) {
+		if (p < 0) return; // -1 表示不存在
+		i64 g = calc_g(p, k);
+		if (g < 0) return;
+		if (ans < 0 || g < ans) ans = g;
+	};
+	upd(p0);
+	upd(p1);
+	return ans;
+}
+
+int main() {
 	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	int T=1;
-//	init();
+	cin.tie(nullptr);
+
+	int T;
 	cin >> T;
 	while (T--) {
-		man();
+		i64 p0, p1, k;
+		cin >> p0 >> p1 >> k;
+		cout << solve_one(p0, p1, k) << '\n';
 	}
 	return 0;
 }
